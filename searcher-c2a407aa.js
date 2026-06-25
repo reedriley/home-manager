ebc9f63c179f6300ec6511b67522ace0f540ac59
reedriley <reedriley@users.noreply.github.com>
2026-06-25 21:04:12 +0000

    deploy: 190a5824b43796c8d27d5a82f744c044d2d04261

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-30f88e46.js',
+                path_to_root + 'searchindex-98b2feea.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
